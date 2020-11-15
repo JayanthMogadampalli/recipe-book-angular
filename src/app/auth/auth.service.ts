a973923fa16e53e6a64f6ebd2e45761dfe06470b
JayanthMogadampalli <jayu6766@gmail.com>
2020-11-16 00:28:51 +0530

    removed api_key

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService{
  signup(email:string, password: string){
    return this.http
    .post<AuthResponseData>(
-    'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDuxoh0vPiQv_hanCdyLVB3EDttR8RpamQ',
+    'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=[API_KEY]',
     {
         email: email,
         password: password,
@@ -49,7 +49,7 @@ export class AuthService{
  login(email:string, password:string){
      return this.http
      .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDuxoh0vPiQv_hanCdyLVB3EDttR8RpamQ',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=[API_KEY]',
         {
             email: email,
             password: password,
